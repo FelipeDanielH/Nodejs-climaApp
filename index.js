a1d1ae153a143a75354294391edd2ce5d2f62c9d
Felipe <felipe.daniel.henriquez@gmail.com>
2024-03-29 16:45:24 -0300

    feat: agrega consulta con la api de mapbox

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const { leerInput, inquirerMenu, pausa, confirmar } = require("./helpers/inquirer")
+require('dotenv').config();
+const { leerInput, inquirerMenu, pausa, confirmar } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
     
+    const busquedas = new Busquedas();
     let opt; 
 
     do {
-        opt = await inquirerMenu()
+        opt = await inquirerMenu();
         switch (opt) {
             case 1:
-                console.log("opcion 1");
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                await busquedas.ciudad(lugar);
+                // Buscar lugares
+                // Seleccionar el lugar
+                // Obntener clima
+
+                console.log('\n Informacion de la ciudad\n');
+                console.log('Ciudad:');
+                console.log('Lat');
+                console.log('Lng');
+                console.log('Temperaura');
+                console.log('Minima');
+                console.log('Maxima');
                 await pausa();
                 break;
             case 2:
-                console.log("opcion 2");
+                
                 await pausa();
                 break;
             case 0:
---
+++ models/busquedas.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+        // TODO leer DB si existe
+    }
+
+    get paramsMapbox() {
+        return {
+            'proximity': 'ip',
+            'access_token': process.env.MAPBOX_KEY,
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
+    async ciudad(lugar = '') {
+
+        try {
+            // peticion http
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+
+            const resp = await instance.get();
+            console.log(resp.data);
+
+            return []; // retornar los lugares
+        } catch (error) {
+            console.log("no se encontro nada", error)
+            return []
+        }
+
+
+
+    }
+}
+
+module.exports = Busquedas;
+
